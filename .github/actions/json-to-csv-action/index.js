5c7f0686db33b10d0eda49a43edf1a1126ae3948
Neha-Wadodkar <neha.wadodkar@omf.com>
2024-12-05 10:13:02 -0600

    Update index.js

--- .github/actions/json-to-csv-action/index.js
+++ .github/actions/json-to-csv-action/index.js
@@ -29,10 +29,10 @@ async function run() {
             Application: deployment.application,
             ObjectName: "",
             version: deployment.version,
-            JiraNumber: deployment.issues.map(issue => issue.key).join('; '),
+            JiraNumber: (deployment.issues || []).map(issue => issue?.key || '').join('; ');
             SourceCodeSystem: "GitHub",
             ApplicationReleaseID: `${deployment.application}-${(deployment.operation.gitHubPullRequest && deployment.operation.gitHubPullRequest.pullNumber) || ''}`,
-            Platform: "MuleSoft"    
+            Platform: ${(deployment.operation.gitHubPullRequest && deployment.operation.gitHubPullRequest.pullNumber) || ''}    
             
         }));
 
